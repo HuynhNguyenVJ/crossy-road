35fdbb8a91f4354b7bb8e00dd5b6740edd3a7d33
Huynh Nguyen <yuyukoeirin@gmail.com>
2025-04-03 16:31:01 +0700

    first commit

---
+++ src/components/Camera.js
@@ -0,0 +1,23 @@
+import * as THREE from "three";
+
+export function Camera() {
+    const SIZE = 300;
+    const VIEW_RATIO = window.innerWidth / window.innerHeight;
+    const WIDTH = VIEW_RATIO < 1 ? SIZE : SIZE * VIEW_RATIO;
+    const HEIGHT = VIEW_RATIO < 1 ? SIZE / VIEW_RATIO : SIZE;
+
+    const camera = new THREE.OrthographicCamera(
+        WIDTH / -2, //left
+        WIDTH / 2, ///right
+        HEIGHT / - 2, ////Top
+        HEIGHT / 2, ////Bottom
+        100, ///Near,
+        900 ///Far
+    )
+
+    camera.up.set(0,0,1);
+    camera.position.set(300, - 300, 300);
+    camera.lookAt(0,0,0);
+
+    return camera;
+}
