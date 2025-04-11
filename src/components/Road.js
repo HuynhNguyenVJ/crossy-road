330c4abbe5981d2ee5b8293e3c0b53ae96045653
Huynh Nguyen <yuyukoeirin@gmail.com>
2025-04-11 15:16:24 +0700

    Add Road Line

--- src/components/Road.js
+++ src/components/Road.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { tileSize, tilesPerRow } from "../constant";
+import { maxTileIndex, tileSize, tilesPerRow } from "../constant";
 
 export function Road(rowIndex){
     const road = new THREE.Group();
@@ -13,5 +13,23 @@ export function Road(rowIndex){
     )
     foundation.receiveShadow = true;
     road.add(foundation);
+
+    const count = maxTileIndex / 2;
+
+    for(let iRow = - count; iRow <= count; iRow++){
+        const roadLine = new THREE.Mesh(
+            new THREE.BoxGeometry(tileSize, 5),
+            new THREE.MeshLambertMaterial({
+                color: 0xeeeeee
+            })
+        )
+        roadLine.position.x = tileSize * iRow * 2;
+        roadLine.position.y = 0;
+        roadLine.position.z = 1;
+        roadLine.receiveShadow = true;
+
+        road.add(roadLine);
+    }
+
     return road;
 }
